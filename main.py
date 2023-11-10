8a7cfec3f0a86239316893bca6455d0c0a46f3e0
IBRAHIMA DIALLO <my@pers.on.al>
2023-11-09 21:37:32 -0500

    Fix CORS issue in main.py

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ allowed_origins = [
     "http://localhost:3001",
 ]
 
-CORS(app, resources={r"/send": {"origins": allowed_origins}})
-# CORS(app, resources={r"/send": {"origins": "*"}})
+CORS(main, resources={r"/send": {"origins": allowed_origins}})
+# CORS(main, resources={r"/send": {"origins": "*"}})
 
 def generate_json(file_name):
     pdf_document = fitz.open(file_name)
